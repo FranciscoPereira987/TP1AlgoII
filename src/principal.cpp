51385c71bace0028df18874726cb96d1fa4d8183
Francisco Javier Pereira <franciscopere987@gmail.com>
2020-10-10 14:35:40 -0300

    Primera subida

---
+++ src/principal.cpp
@@ -0,0 +1,13 @@
+//
+// Created by francisco on 10/10/20.
+//
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+
+    cout << "Se empezo el TP1!!!" << endl;
+
+    return 0;
+}
